0be703f0d5e4b8bbf99e32b8c0b445b5a6d528c6
Dillan Cagnetta <dillancagnetta@yahoo.com>
2021-02-25 20:15:48 +0200

    Fix: Enumeration

--- src/Codeboss/src/Codeboss/Types/Enumeration.cs
+++ src/Codeboss/src/Codeboss/Types/Enumeration.cs
@@ -1,23 +1,16 @@
-﻿using System;
-
-namespace Codeboss.Types
+﻿namespace Codeboss.Types
 {
     public interface IEnumeration<TType>
     {
         TType Value { get; set; }
     }
 
-    public class Enumeration<TEnumeration, TType> : IEnumeration<TType> 
-        where TEnumeration : class
-        where TType: class
+    public class Enumeration<TEnumeration, TType> : IEnumeration<TType>
     {
         public TType Value { get; set; }
 
         // Cast Conversion Operator Overloading
         public static implicit operator string(Enumeration<TEnumeration, TType> x) => x.Value.ToString();
-
-        // Assignment Operator  Overloading
-        public static implicit operator Enumeration<TEnumeration, TType>(string value) => new(){Value = value as TType};
 
         public override string ToString() => Value.ToString();
     }
