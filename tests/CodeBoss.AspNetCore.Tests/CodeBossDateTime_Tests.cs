fcfcb4e5fb080261d177ecb39ce9c5a45c1c6a33
Dillan Cagnetta <dillancagnetta@yahoo.com>
2021-10-15 21:30:32 +0200

    Added: GetMonthName enum

--- tests/CodeBoss.AspNetCore.Tests/CodeBossDateTime_Tests.cs
+++ tests/CodeBoss.AspNetCore.Tests/CodeBossDateTime_Tests.cs
@@ -1,5 +1,6 @@
 using System;
 using CodeBoss.AspNetCore.CbDateTime;
+using CodeBoss.Extensions;
 using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.Options;
 using Xunit;
@@ -11,7 +12,7 @@ namespace CodeBoss.AspNetCore.Tests
         [Fact]
         public void Should_convert_from_Utc_to_timezone_date()
         {
-            var options = Options.Create(new DateTimeOptions {TimeZone = "South Africa Standard Time"});
+            var options = Options.Create(new DateTimeOptions { TimeZone = "South Africa Standard Time" });
             var dateTimeProvider = new CodeBossDateTimeProvider(options, new NullLogger<CodeBossDateTimeProvider>());
 
             var utcDateTime = DateTime.UtcNow;
@@ -19,6 +20,17 @@ namespace CodeBoss.AspNetCore.Tests
 
             var utcToTimeZone = utcDateTime.AddHours(+2);
             Assert.Equal(utcToTimeZone, timeZoneDateTime);
+        }
+
+
+        [Fact]
+        public void Should_calculate_difference_between_start_and_end_of_weeks()
+        {
+            var from = DateTime.UtcNow.StartOfWeek(DayOfWeek.Monday);
+            var to = DateTime.UtcNow.EndOfWeek(DayOfWeek.Monday);
+
+            Assert.True(from.DayOfWeek == DayOfWeek.Monday);
+            Assert.True(to.DayOfWeek == DayOfWeek.Sunday);
         }
     }
 }
